8918b4d36425be933f37cf45e3729be8ac4edcf5
LaisNSilva <laisns@al.insper.edu.br>
2019-04-23 21:30:12 -0300

    adicionando asteroides

--- asteroids-base.py
+++ asteroids-base.py
@@ -3,6 +3,7 @@
 # Importando as bibliotecas necessárias.
 import pygame
 from os import path
+import random 
 
 # Estabelece a pasta que contem as figuras.
 img_dir = path.join(path.dirname(__file__), 'img')
@@ -37,14 +38,27 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.transform.scale(player_img, (50, 38))
         
         # Deixando transparente.
-        self.image.set_colorkeys(BLACK)
+        self.image.set_colorkey(BLACK)
         
         # Detalhes sobre o posicionamento.
         self.rect = self.image.get_rect()
         
         #Centraliza embaixo da tela.
         self.rect.centerx = WIDTH / 2
-        self.recet.bottom = HEIGHT - 10
+        self.rect.bottom = HEIGHT - 10
+        
+        # Velocidade da nave
+        self.speedx = 0
+    #Metodo que atualiza a posição da navinha
+    def update(self):
+        self.rect.x += self.speedx
+        
+        #Mantem dentro da tela
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
+            
     
 
 # Inicialização do Pygame.
@@ -57,6 +71,9 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 # Nome do jogo
 pygame.display.set_caption("Asteroids")
 
+#Variavel para o ajuste de velocidade
+clock = pygame.time.Clock()
+
 # Carrega o fundo do jogo
 background = pygame.image.load(path.join(img_dir, 'starfield.png')).convert()
 background_rect = background.get_rect()
@@ -68,6 +85,47 @@ player = Player()
 all_sprites = pygame.sprite.Group()
 all_sprites.add(player)
 
+class Mob(pygame.sprite.Sprite):
+    
+    # Construtor da classe.
+    def __init__(self):
+    
+        # Costrutor da classe pai (Sprite).
+        pygame.sprite.Sprite.__init__(self)
+    
+        # Carregando a imagem de fundo.
+        player_img = pygame.image.load(path.join(img_dir, "meteorBrown_med1.png")).convert()
+        self.image = player_img
+        
+        # Diminuindo o tamanho da imagem.
+        #self.image = pygame.transform.scale(player_img, (50, 38))
+        
+        # Deixando transparente.
+        self.image.set_colorkey(BLACK)
+        
+        # Detalhes sobre o posicionamento.
+        self.rect = self.image.get_rect()
+        
+        #Centraliza embaixo da tela.
+        self.rect.centerx = random.randrange(0, WIDTH)
+        self.rect.bottom = random.randrange(-100, -40)
+        
+        # Velocidade da nave
+        self.speedx = random.randrange(-3, 3)
+        self.speedy = random.randrange(2, 9)
+
+mobs = pygame.sprite.Group()
+
+i = 0
+while i < 8:        
+    #Criar uma nave. O construtor será chamado automaticamente
+    mob = Mob()
+    #Criar um grupo de sprites e adiciona a nave
+    mobs.add(mob)
+    all_sprites.add(mob)
+    
+    i+=1    
+    
 # Comando para evitar travamentos.
 try:
     
@@ -75,12 +133,36 @@ try:
     running = True
     while running:
         
+        #Ajusta a velocidade do jogo.
+        clock.tick(FPS)
+        
         # Processa os eventos (mouse, teclado, botão, etc).
         for event in pygame.event.get():
             
             # Verifica se foi fechado
             if event.type == pygame.QUIT:
                 running = False
+            
+            #Verifica se apertoou alguma tecla.
+            if event.type == pygame.KEYDOWN:
+                #Dependendo da tecla, altera a velocidade.
+                if event.key == pygame.K_LEFT:
+                    player.speedx = - 8
+                if event.key == pygame.K_RIGHT:
+                    player.speedx = 8
+            
+            #Verifica se soltou alguma tecla.
+            if event.type == pygame.KEYUP:
+                #Dependendo da tecla, altera a velocidade.
+                if event.key == pygame.K_LEFT:
+                    player.speedx = 0
+                if event.key == pygame.K_RIGHT:
+                    player.speedx = 0
+            
+        # Depois de processar os eventos.
+        # Atualizar a acao de cada sprite.
+        all_sprites.update()
+                
     
         # A cada loop, redesenha o fundo e os sprites
         screen.fill(BLACK)
